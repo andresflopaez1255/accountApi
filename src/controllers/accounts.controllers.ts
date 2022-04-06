aa940ff1cca653c6de0641e3a0623f4d8e660b86
andres florez <andresflorez@MBP-de-andres.domain.name>
2022-04-05 19:35:03 -0500

    prueba

--- src/controllers/accounts.controllers.ts
+++ src/controllers/accounts.controllers.ts
@@ -3,27 +3,15 @@
 /* import { categories_account, Prisma, user } from '@prisma/client';
  */ import { Request, Response } from 'express';
 import moment from 'moment';
-import { infoMessage } from '../Interfaces';
 import prisma from '../utils/dbClient';
 import messageBody from '../utils/messageBody';
 import { MessagesAccounts } from '../utils/messages';
-import sendMessage from './messages.controller';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function getAllAccounts(
 	req: Request,
 	res: Response
 ): Promise<Response<any, Record<string, any>> | undefined> {
-	const result = await getAccountsWithDate();
-
-	if (result.length == 0) {
-		return;
-	}
-	result.map((info: infoMessage) => {
-		sendMessage(info);
-	});
-
-	console.log(result);
 	try {
 		const allAccounts = await prisma.accounts.findMany();
 		const dataAccounts: any = [];
@@ -74,13 +62,14 @@ async function getAllAccounts(
 
 async function getAccountsWithDate() {
 	try {
-		const date = moment().format('L');
-		const after2daysFromToday = moment().add(1, 'days').format('L');
-		console.log(after2daysFromToday);
+
+		const date = moment().format('L')
+		const after2daysFromToday = moment().add(1,'days').format('L')
+		console.log(after2daysFromToday)
 		const allAccounts: any = await prisma.$queryRaw`
          SELECT * FROM accounts WHERE  expiration_date >= ${date} AND expiration_date <= ${after2daysFromToday}
         `;
-		console.log('okiii', allAccounts);
+		console.log('okiii', allAccounts)
 		const dataAccounts: any = [];
 		for (let index = 0; index < allAccounts.length; index++) {
 			const account = allAccounts[index];
@@ -107,7 +96,7 @@ async function getAccountsWithDate() {
 				name_user: userInfo?.name_user,
 				cellphone_user: userInfo?.cellphone_user,
 				category: categoryInfo?.category_name,
-			});
+			})
 			dataAccounts.push({
 				...account,
 				name_user: userInfo?.name_user,
@@ -122,58 +111,60 @@ async function getAccountsWithDate() {
 	}
 }
 
-async function addAccount(req: Request, res: Response) {
-	const account = req.body;
+async function addAccount(req:Request, res: Response) {
+	const account = req.body
 	try {
 		const AccountIfExist = prisma.users.findFirst({
-			where: account.id,
-		});
-		if (!AccountIfExist) {
+			where: account.id
+		})
+		if(!AccountIfExist){
 			await prisma.categories_account.create({
-				data: account,
-			});
-
-			res.status(200).json(messageBody(null, MessagesAccounts.created, true));
+				data:account
+			}); 
+			
+			res.status(200).json(messageBody(null,MessagesAccounts.created,true))
 		}
-		res.status(202).json(messageBody(null, 'category if exist', false));
+		res.status(202).json(messageBody(null,'category if exist',false))
 	} catch (error) {
-		res.status(401).json(messageBody(error, MessagesAccounts.error, false));
+		res.status(401).json(messageBody(error,MessagesAccounts.error,false))
 	}
 }
 
-async function updateAccount(req: Request, res: Response) {
+async function updateAccount(req:Request, res:Response) {
+	
+	
 	try {
+		
 		const result = await prisma.accounts.update({
 			where: {
-				id: parseInt('' + req.query.id),
+				id: parseInt('' + req.query.id)
 			},
-			data: req.body,
-		});
-		res.status(200).json(messageBody(result, MessagesAccounts.updated, true));
+			data: req.body
+		})
+		res.status(200).json(messageBody(result,MessagesAccounts.updated,true))
 	} catch (error) {
-		res.status(400).json(messageBody(error, MessagesAccounts.error, false));
+		res.status(400).json(messageBody(error,MessagesAccounts.error,false))
 	}
 }
 
-async function deleteAccount(req: Request, res: Response) {
-	console.log(req.query.id);
+async function deleteAccount(req:Request,res:Response) {
+	console.log( req.query.id)
 	try {
+		
+
 		const result = await prisma.accounts.delete({
 			where: {
-				id: parseInt('' + req.query.id),
-			},
-		});
+				id:  parseInt(''+req.query.id)
+			}
 
-		res.status(200).json(messageBody(result, MessagesAccounts.deleted, true));
+		})
+
+		
+		res.status(200).json(messageBody(result,MessagesAccounts.deleted,true))
 	} catch (error) {
-		res.status(400).json(messageBody(error, MessagesAccounts.error, false));
+		res.status(400).json(messageBody(error,MessagesAccounts.error,false))
 	}
 }
 
-export {
-	getAllAccounts,
-	getAccountsWithDate,
-	addAccount,
-	updateAccount,
-	deleteAccount,
-};
+
+export { getAllAccounts, getAccountsWithDate,addAccount,updateAccount,deleteAccount };
