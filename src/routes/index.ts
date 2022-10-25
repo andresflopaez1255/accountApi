62e5c41a2da95a0dc26a9c8183e2752c2266ef31
andres florez <andresflopaez@gmail.com>
2022-10-25 17:08:02 -0500

    fixed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -17,5 +17,5 @@ router.get(Routes.categories, getCategories);
 //accounts endpoints
 router.get(Routes.accounts, getAllAccounts);
 router.post(Routes.addAccount,jsonParser, addAccount)
-router.delete(`${Routes.deleteAccount}/:id`,deleteAccount)
+router.get(`${Routes.deleteAccount}/:id`,deleteAccount)
 export default router;
