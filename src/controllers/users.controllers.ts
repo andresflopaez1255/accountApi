76c3952ecf4b006c038dca51a855210cfc6e3d07
Andres florez <andresflopaez@gmail.com>
2025-05-15 19:29:35 -0500

    fix: remove whitelisted domain from CORS configuration

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,108 +1,103 @@
-import { Request, Response } from 'express';
-import prisma from '../utils/dbClient';
-import messageBody from '../utils/messageBody';
-import { MessagesUsers } from '../utils/messages';
-import { db } from '../firebase';
-
+import { Request, Response } from "express";
+import prisma from "../utils/dbClient";
+import messageBody from "../utils/messageBody";
+import { MessagesUsers } from "../utils/messages";
+import { db } from "../firebase";
+import { v4 as uuid } from "uuid";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
-
 async function getUsers(
-	req: Request,
-	res: Response
+  req: Request,
+  res: Response
 ): Promise<Response<any, Record<string, any>> | undefined> {
-	res.setHeader('Content-Type', 'application/json');
-	const usersSnapshot = await db.collection('users').get();
-	const users = usersSnapshot.docs.map(doc => doc.data());
-	try {
-
-		if (!users.length) {
-			return res
-				.status(200)
-				.send(messageBody(users, MessagesUsers.notSuccessful, true));
-		} else {
-			return res
-				.status(200)
-				.send(messageBody(users, MessagesUsers.successful, true));
-		}
-	} catch (error) {
-		console.log(error); // Mueve esto aquí
-		return res.status(400).send(messageBody(null, MessagesUsers.error, true));
-	}
+  res.setHeader("Content-Type", "application/json");
+  const usersSnapshot = await db.collection("users").get();
+  const users = usersSnapshot.docs.map((doc) => doc.data());
+  try {
+    if (!users.length) {
+      return res
+        .status(200)
+        .send(messageBody(users, MessagesUsers.notSuccessful, true));
+    } else {
+      return res
+        .status(200)
+        .send(messageBody(users, MessagesUsers.successful, true));
+    }
+  } catch (error) {
+    console.log(error); // Mueve esto aquí
+    return res.status(400).send(messageBody(null, MessagesUsers.error, true));
+  }
 }
-
 
 async function addUser(req: Request, res: Response) {
-	const user = req.body;
-	res.setHeader('Content-Type', 'application/json');
-	try {
-		const userIfExist = await prisma.users.findFirst({
-			where: {
-				cellphone_user: user.cellphone_user
-			}
-		});
+  const user = req.body;
+  res.setHeader("Content-Type", "application/json");
+  const userIfExist = await db
+    .collection("users")
+    .doc(user.cellphone_user)
+    .get();
 
-		if (!userIfExist) {
-			await prisma.users.create({
-				data: user
-			});
-			res.status(200).json(messageBody(null, MessagesUsers.created, true));
-		} else {
-			res.status(400).json(messageBody(null, 'El usuario ya existe', true));
-		}
-	} catch (error) {
-		console.error(error);
-		res.status(401).json(messageBody(error, MessagesUsers.error, false));
-	}
+  try {
+    if (!userIfExist.exists) {
+      // buscar cuentas por id de usuario en firebase en accounts
+      const accounts = await db
+        .collection("accounts")
+        .where("id_user", "==", user.user_id)
+        .get();
+
+      db.collection("users").add({
+        id_user: uuid(),
+        ...user,
+        accounts: accounts.docs.length > 0 ? accounts.docs : [],
+      });
+
+      res.status(200).json(messageBody(null, MessagesUsers.created, true));
+    } else {
+      res.status(400).json(messageBody(null, "El usuario ya existe", true));
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(401).json(messageBody(error, MessagesUsers.error, false));
+  }
 }
 
-async function updateUser(req:Request, res:Response) {
+async function updateUser(req: Request, res: Response) {
+  res.setHeader("Content-Type", "application/json");
 
-	res.setHeader('Content-Type', 'application/json');
+  try {
+    const result = await prisma.users.update({
+      where: {
+        id: parseInt("" + req.body.id),
+      },
+      data: req.body,
+    });
 
-	
-	try {
-		
-		const result = await prisma.users.update({
-			where: {
-				id: parseInt('' + req.body.id)
-			},
-			data: req.body
-		})
-		
-
-		res.status(200).json(messageBody(result,MessagesUsers.updated,true))
-	} catch (error) {
-		res.status(400).json(messageBody(error,MessagesUsers.error,false))
-	}
+    res.status(200).json(messageBody(result, MessagesUsers.updated, true));
+  } catch (error) {
+    res.status(400).json(messageBody(error, MessagesUsers.error, false));
+  }
 }
 
-async function deleteUser(req:Request,res:Response) {
+async function deleteUser(req: Request, res: Response) {
+  const id = req.query.id?.toString() ?? "";
+  res.setHeader("Content-Type", "application/json");
 
-	const id = req.query.id?.toString() ?? ''
-	res.setHeader('Content-Type', 'application/json');
+  try {
+    const result = await prisma.users.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
 
-	try {
-		
+    await prisma.accounts.deleteMany({
+      where: {
+        id: parseInt(id),
+      },
+    });
 
-		const result = await prisma.users.delete({
-			where: {
-				id: parseInt(id)
-			}
-
-		})
-
-		await prisma.accounts.deleteMany({
-			where: {
-				id:  parseInt(id)
-			}
-
-		})
-		
-
-		res.status(200).json(messageBody(result,MessagesUsers.deleted,true))
-	} catch (error) {
-		res.status(400).json(messageBody(error,MessagesUsers.error,false))
-	}
+    res.status(200).json(messageBody(result, MessagesUsers.deleted, true));
+  } catch (error) {
+    res.status(400).json(messageBody(error, MessagesUsers.error, false));
+  }
 }
-export { getUsers, addUser, updateUser, deleteUser};
+export { getUsers, addUser, updateUser, deleteUser };
