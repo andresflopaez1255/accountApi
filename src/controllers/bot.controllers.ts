01d287a81ab7cd5f8557875c0c947730c0799635
andresflopaez1255 <andresflopaez@gmail.com>
2025-11-12 12:09:58 -0500

    fixed

--- src/controllers/bot.controllers.ts
+++ src/controllers/bot.controllers.ts
@@ -369,20 +369,21 @@ export const managerBotController = async (app: Express) => {
 
 
 		}
+		bot.telegram.setMyCommands([
+			{ command: 'crear_cuenta', description: 'Crear una nueva cuenta' },
+			{ command: 'actualizar_cuenta', description: 'Ver todas las cuentas' },
+			{ command: 'listar_cuentas', description: 'Ver todas las cuentas' },
+			{ command: 'garantia', description: 'actualizar cuenta  por garantia' },
+			{ command: 'buscar_cuentas', description: 'Buscar cuentas por correo o cliente' },
+			{ command: 'proximos_vencer', description: 'Proximas cuentas a vencer' },
+			{ command: 'cancelar', description: 'Cancelar el proceso actual' },
+			{ command: 'ayuda', description: 'Mostrar los comandos disponibles' },
+		]);
 		userSessions[ctx.chat.id] = undefined;
 	});
 
 
-	bot.telegram.setMyCommands([
-		{ command: 'crear_cuenta', description: 'Crear una nueva cuenta' },
-		{ command: 'actualizar_cuenta', description: 'Ver todas las cuentas' },
-		{ command: 'listar_cuentas', description: 'Ver todas las cuentas' },
-		{ command: 'garantia', description: 'actualizar cuenta  por garantia' },
-		{ command: 'buscar_cuentas', description: 'Buscar cuentas por correo o cliente' },
-		{ command: 'proximos_vencer', description: 'Proximas cuentas a vencer' },
-		{ command: 'cancelar', description: 'Cancelar el proceso actual' },
-		{ command: 'ayuda', description: 'Mostrar los comandos disponibles' },
-	]);
+	
 
 
 
